e1a54b36b6a1b8ccd410f038a22e31a278ebc79b
aniketpinomic <109971668+aniketpinomic@users.noreply.github.com>
2022-08-08 10:27:19 +0530

    Add files via upload

--- SecondProblem.java
+++ SecondProblem.java
@@ -5,7 +5,7 @@ public class SecondProblem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int number = 501;			//Write any number hear 
+		int number = 501;			                //Write any number here 
 		String possitiveResult="possitive";
 		String negativeResult="negative";
 		if(number>0) {
@@ -19,13 +19,13 @@ public class SecondProblem {
 		System.out.println("---------------------------------------------------");
 		
 		
-		int divident = 2;
+		int dividend = 2;
 		String two = new String("two");
-		if(number%divident==0){
-			System.out.println("The number is devisible by"+" "+two.substring(0,1).toUpperCase()+two.substring(1));
+		if(number%dividend==0){
+			System.out.println("The number is divisible by"+" "+two.substring(0,1).toUpperCase()+two.substring(1));
 		}
 		else {
-			System.out.println("The number dose not devisible by"+" "+two.substring(0,1).toUpperCase()+two.substring(1));
+			System.out.println("The number dose not divisible by"+" "+two.substring(0,1).toUpperCase()+two.substring(1));
 		}
 	}
 
