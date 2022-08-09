b5cbaff672e726fc47b38dbec716fcca1cb36a94
aniketpinomic <109971668+aniketpinomic@users.noreply.github.com>
2022-08-09 21:02:25 +0530

    Add files via upload

---
+++ FourthProblem.java
@@ -0,0 +1,20 @@
+package project1;
+
+public class FourthProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int anyNumber=0;
+		if(anyNumber!=0) {
+			if(anyNumber==(anyNumber/2)+(anyNumber/2)) {
+				System.out.println("The given number is Even");
+			}
+			else if(anyNumber!=(anyNumber/2)+(anyNumber/2)) {
+				System.out.println("The given number is odd");
+			}
+		}
+		else {
+			System.out.println("The given number is nither Even nor Odd that is,it is Zero");
+		}
+	}
+}
